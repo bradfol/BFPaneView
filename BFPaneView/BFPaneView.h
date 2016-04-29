5b2b730f5c528b4c17b9fb60fa61f69685092602
Brad Fol <bradfol@gmail.com>
2016-04-29 10:15:44 -0700

    Initial commit

---
+++ BFPaneView/BFPaneView.h
@@ -0,0 +1,19 @@
+//
+//  BFPaneView.h
+//  BFPaneView
+//
+//  Created by Brad Fol on 4/29/16.
+//  Copyright © 2016 Brad Fol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BFPaneView.
+FOUNDATION_EXPORT double BFPaneViewVersionNumber;
+
+//! Project version string for BFPaneView.
+FOUNDATION_EXPORT const unsigned char BFPaneViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BFPaneView/PublicHeader.h>
+
+
